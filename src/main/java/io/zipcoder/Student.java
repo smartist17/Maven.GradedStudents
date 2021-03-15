ee661abc922dc04fc38ea48b8abe62bfbadb6805
smartist17 <79124739+smartist17@users.noreply.github.com>
2021-03-15 10:42:37 -0400

    work in progress

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,47 @@
 package io.zipcoder;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Student {
+
+    String firstName;
+    String lastName;
+    ArrayList<Double> testScores;
+
+    public Student(String firstName, String lastName, Double[] testScores){}
+
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public ArrayList<Double> getTestScores(){}
+
+    public int getNumberOfExamsTaken(){}
+
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public void addExamScores(double examScore){}
+
+    public void setExamScore(int examNumber, double newScore){}
+
+    public Double getAverageExamScore(){}
+
+    public String toString(){}
+
+
+
+
+
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,60 @@
 package io.zipcoder;
 
-public class StudentTest {
+import org.junit.Test;
 
+import java.util.ArrayList;
+
+public class StudentTest {
+    @Test
+    public void defaultConstructorTest() {
+        String firstName = "Ashley";
+        String lastName = "Smart";
+        Double[] testScores = {100.0, 90.0, 80.0, 70.0};
+
+
+    }
+
+    @Test
+    public void getFirstName() {
+        Double[] testScores = {100.0, 90.0, 80.0, 70.0};
+        Student testStudent = new Student("Ashley","Smart",testScores);
+        testStudent.getFirstName();
+        String actualFirstName = testStudent.getFirstName();
+    }
+
+    @Test
+    public void getLastName() {
+    }
+
+    @Test
+    public void getTestScores() {
+    }
+
+    @Test
+    public void getNumberOfExamsTaken() {
+    }
+
+    @Test
+    public void setFirstName() {
+    }
+
+    @Test
+    public void setLastName() {
+    }
+
+    @Test
+    public void addExamScores() {
+    }
+
+    @Test
+    public void setExamScore() {
+    }
+
+    @Test
+    public void getAverageExamScore() {
+    }
+
+    @Test
+    public void testToString() {
+    }
 }
